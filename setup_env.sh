022ff568f721311bab5425952b82fba90750ef8a
Chrixco <christianxcorral@gmail.com>
2025-05-26 18:09:39 +0200

    website first draft

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+# setup_env.sh
+#!/bin/bash
+
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Environment setup complete. Use 'source venv/bin/activate' to activate."
